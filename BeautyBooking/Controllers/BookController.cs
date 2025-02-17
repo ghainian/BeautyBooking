8fdd3f9a2fff388d3e443f9ec6d66c8cfd1aa804
mehran ghainian <ghainian@gmail.com>
2025-02-17 22:31:21 +0100

    latest add book

---
+++ BeautyBooking/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+using BeautyBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BeautyBooking.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("book");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
